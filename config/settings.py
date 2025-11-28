82a1bc27125695baaa8670bf4c403f5b5c4807aa
CupidoUP <cupidoup1@gmail.com>
2025-11-27 22:33:15 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
 # -------------------------
 # Redis / Cache
 # -------------------------
-REDIS_URL = os.getenv("REDIS_URL")                                
+REDIS_URL = os.getenv("REDIS_URL", "redis://default:tetroliamosxd@190.90.114.214:6379")                             
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
